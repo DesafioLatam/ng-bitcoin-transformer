99b73ac20125d50fed4489c89b590bcb008070fb
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-07-17 20:13:42 -0500

    mini example for transforming bitcoins to dollars

--- app.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('app', []);
-	app.controller('PhotoController', ['$scope', function ($scope) {
-		$scope.a = 1
-		$scope.incrementar = function(){
-			$scope.a = $scope.a + 1
-		} 
-}]);
---
+++ public/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('app', []);
+	app.controller('BitController', ['$scope', '$http', function ($scope, $http) {
+		$scope.results = "";
+		$scope.getPrice = function()
+		{
+			console.log("hola");
+			options = {
+				url: "https://montanaflynn-bitcoin-exchange-rate.p.mashape.com/prices/buy",
+				method: "GET",
+				headers: {
+					'X-Mashape-Key':'yvU0GDeVzomshLIpyOJyuOq7BQxbp1k6xcGjsnerwWf9tuSgD9',
+					"Accept": "text/plain"
+				},
+				data: {
+					qty: '5',
+				}
+			};
+
+			$http(options)
+			.success(function(data){
+				console.log($scope.bitcoins)
+				$scope.results = data
+				$scope.price = data.amount
+				console.log(data);
+			})
+			.error(function(data){
+				console.log("error")
+			})
+
+		}
+
+}]);
